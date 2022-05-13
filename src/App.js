444454b4fba247f111901613836e00704a5b8a53
Dgirunies <erineu16@gmail.com>
2022-05-13 11:49:20 +0100

    Adding Modal Components and FetchLanguages Method

--- src/App.js
+++ src/App.js
@@ -6,8 +6,33 @@ import Button from "./components/Button";
 import Modal from "./components/Modal";
 
 const App = () => {
+  const [showModal, setShowModal] = useState(null);
   const [inputLanguage, setInputLanguage] = useState("English");
   const [outputLanguage, setOutputLanguage] = useState("Polish");
+
+  const getLanguages = async () => {
+    const axios = require("axios");
+
+    const options = {
+      method: "GET",
+      url: "https://google-translate20.p.rapidapi.com/languages",
+      headers: {
+        "X-RapidAPI-Host": "google-translate20.p.rapidapi.com",
+        "X-RapidAPI-Key": "",
+      },
+    };
+
+    console.log(process.env);
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
   const arrowLanguageSwitcherHandler = () => {
     const tmpInputLanguage = inputLanguage;
@@ -17,11 +42,27 @@ const App = () => {
 
   return (
     <div className="app">
-      <TextBox selectedLanguage={inputLanguage} style="input" />
-      <div className="arrow-container" onClick={arrowLanguageSwitcherHandler}>
-        <Arrows />
-      </div>
-      <TextBox selectedLanguage={outputLanguage} style="output" />
+      {!showModal && (
+        <>
+          <TextBox
+            selectedLanguage={inputLanguage}
+            style="input"
+            setShowModal={setShowModal}
+          />
+          <div
+            className="arrow-container"
+            onClick={arrowLanguageSwitcherHandler}
+          >
+            <Arrows />
+          </div>
+          <TextBox
+            selectedLanguage={outputLanguage}
+            style="output"
+            setShowModal={setShowModal}
+          />
+        </>
+      )}
+      {showModal && <Modal setShowModal={setShowModal} />}
     </div>
   );
 };
